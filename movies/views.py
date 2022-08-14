03364ce42b367319ad988e5378680ed2387ff99e
Nikoloz97 <Nick.gotsy@gmail.com>
2022-08-14 11:48:26 -0400

    Configured url

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+# When user sends http request at endpoint (URL), index function = returns response
+def index(request):
+    return HttpResponse("Hello There")
--- vidly/urls.py
+++ vidly/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+# imported function: include
+from django.urls import path, include
 
+# Add the movies path
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # any path that starts with movies/ = handed off to URL config in app "movies"
+    path('movies/', include('movies.url'))
 ]
